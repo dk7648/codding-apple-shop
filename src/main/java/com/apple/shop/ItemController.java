a20323fe75641e729ac20b51f4eb33288d97aed3
dk7648 <dk7648@korea.ac.kr>
2024-12-08 18:08:37 +0900

    feat: 수정기능

--- src/main/java/com/apple/shop/ItemController.java
+++ src/main/java/com/apple/shop/ItemController.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ItemController {
     private final ItemRepository itemRepository;
+    private final ItemService itemService;
 
     @GetMapping("/list")
     String list(Model model) {
@@ -29,44 +30,29 @@ public class ItemController {
     @PostMapping("/add")
     String addPost(@RequestParam String title, @RequestParam int price) {
         //@ModelAttribute Item item으로도 받아올 수 있다.
-        System.out.println(title);
-        System.out.println(price);
-        Item newItem = new Item();
-        newItem.setTitle(title);
-        newItem.setPrice(price);
-        itemRepository.save(newItem);
+        itemService.saveItem(title, price);
         return "redirect:/list";
     }
 
-//    @GetMapping("/detail/{id}")
-//    String detail(@PathVariable Long id, Model model) {
-//        Optional<Item> result = itemRepository.findById(id);
-//        if (result.isPresent()) {
-//            System.out.println(result.get());
-//            model.addAttribute("item", result.get());
-//            return "detail.html";
-//        }
-//        return "redirect:/list";
-//    }
-
-    //rest api의 경우
     @GetMapping("/detail/{id}")
     String detail(@PathVariable Long id, Model model) {
-        try {
-            Optional<Item> result = itemRepository.findById(id);
-            System.out.println(result.get());
-            model.addAttribute("item", result.get());
-            return "detail.html";
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return "redirect:/list";
-        }
+        Item item = itemService.findItem(id);
+        model.addAttribute("item", item);
+        return "detail.html";
     }
 
-    //같은 페이지에서 발생하는 모든 에러를 이곳에서 처리
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<String> handler() {
-//        return ResponseEntity.status(400).body("에러");
-//    }
+    @GetMapping("edit/{id}")
+    String edit(@PathVariable Long id, Model model) {
+        Item item = itemService.findItem(id);
+        model.addAttribute("item", item);
+        return "edit.html";
+    }
+
+    @PostMapping("/edit")
+    String addPost(@RequestParam Long id, @RequestParam String title, @RequestParam int price) {
+        //@ModelAttribute Item item으로도 받아올 수 있다.
+        itemService.editItem(id, title, price);
+        return "redirect:/list";
+    }
 }
 
---
+++ src/main/java/com/apple/shop/ItemService.java
@@ -0,0 +1,46 @@
+package com.apple.shop;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ItemService {
+    private final ItemRepository itemRepository;
+    public void saveItem(String title, int price) {
+        validateItem(title, price);
+        Item newItem = new Item();
+        newItem.setTitle(title);
+        newItem.setPrice(price);
+        itemRepository.save(newItem);
+    }
+
+    public void editItem(Long id, String title, int price) {
+        validateItem(title, price);
+        Item newItem = new Item();
+        newItem.setId(id);
+        newItem.setTitle(title);
+        newItem.setPrice(price);
+
+        itemRepository.save(newItem);
+    }
+
+    private void validateItem(String title, int price) {
+        if(price < 0) {
+            throw new IllegalArgumentException("가격은 0원 이상이어야 합니다.");
+        }
+        if(title.length() > 100) {
+            throw new IllegalArgumentException("상품명은 100자를 넘을 수 없습니다.");
+        }
+    }
+
+    public Item findItem(Long id) {
+        Optional<Item> result = itemRepository.findById(id);
+        if(result.isPresent()) {
+            return result.get();
+        }
+        throw new IllegalArgumentException("해당하는 제품을 찾을 수 없습니다.");
+    }
+}
